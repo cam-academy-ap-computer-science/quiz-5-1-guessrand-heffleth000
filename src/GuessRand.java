4830620316eb9c31958aea1ba42b89069e7c16f7
heffleth000 <heffleth000@CAM-LAB-15.bgsd.k12.wa.us>
2019-11-15 07:48:18 -0800

    Idk if you're still accepting but I missed a bracket yay

--- src/GuessRand.java
+++ src/GuessRand.java
@@ -7,14 +7,14 @@ public class GuessRand {
 		Random r = new Random();
 		rand = r.nextInt(100) + 1;
 		
-		while (success = false) {
+		while (success == false) {
 			int guess = 0;
 			Scanner userInput = new Scanner (System.in);
 			System.out.println("Your Guess: ?");
 			guess = userInput.nextInt(); 
 			success = compare(guess, rand); // call 'compare()' method
 		}
-
+	}
 	public static boolean compare(int guess, int rand) {
 		boolean got_it = false;
 		if (guess == rand) {  // Fix if test
